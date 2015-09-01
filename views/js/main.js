9048aaa62664bfbb056b1b26a2353a83f115370d
Samir Mehta <sammehta88@gmail.com>
2015-08-31 23:52:51 -0400

    FIX: Change phase to phaseArray

--- views/js/main.js
+++ views/js/main.js
@@ -520,7 +520,7 @@ function updatePositions() {
   var phase;
 
   for (var j = 0; j < 5; j++) {
-    phase[j] = Math.sin(scroll + j);
+    phaseArray[j] = Math.sin(scroll + j);
   }
 
   for (var i = 0; i < itemsLength; i++) {
