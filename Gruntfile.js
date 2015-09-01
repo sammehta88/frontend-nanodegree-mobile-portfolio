523c21738797ec99cef91f4d7c6b0a01a2aff7d2
Samir Mehta <sammehta88@gmail.com>
2015-08-31 20:04:55 -0400

    Optimize images, Minify CSS and JS, Create gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,107 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    //css minifier, not configured to combine files, change if needed https://github.com/gruntjs/grunt-contrib-cssmin
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          src: ['css/*.css'],
+          dest: 'release',
+          ext: '.min.css'
+        }]
+      }
+    },
+    //JS minifier  (https://github.com/gruntjs/grunt-contrib-uglify)
+    uglify: {
+      target: {
+        files: {
+          'release/js/perfmatters.min.js': ['js/perfmatters.js'],
+        }
+      }
+    },
+    //https://github.com/gruntjs/grunt-contrib-imagemin
+    imagemin: {
+      main: {
+        //only for png?
+        options: {
+          optimizationLevel: 3,
+        },
+        files: [{
+          expand: true,
+          cwd: 'img/',
+          src: ['*.{png,jpg}'],
+          dest: 'release/img'
+        }]
+      },
+      pizza: {
+        options: {
+          optimizationLevel: 5,
+        },
+        files: {
+          'views/release/images/pizzeria-large.jpg': 'views/images/pizzeria-large.jpg',
+          'views/release/images/pizzeria-med.jpg': 'views/images/pizzeria-med.jpg',
+          'views/release/images/pizzeria-small.jpg': 'views/images/pizzeria-small.jpg',
+          'views/release/images/pizza.png': 'views/images/pizza.png'
+        }
+      }
+    },
+    //https://github.com/andismith/grunt-responsive-images
+    responsive_images: {
+      target: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            name: 'small',
+            width: 117,
+            quality: 20
+          },{
+            name: 'large',
+            width: 720,
+            quality: 20
+          },{
+            name: 'med',
+            width: 293,
+            quality: 20
+          }]
+        },
+        files: [{
+          expand: true,
+          cwd: 'views/',
+          src: ['images/*.jpg'],
+          dest: 'views/'
+        }]
+      }
+    },
+    //TODO: JSHint and watch
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'qunit']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-responsive-images');
+
+  grunt.registerTask('test', ['jshint']);
+
+  grunt.registerTask('default', ['jshint', 'cssmin', 'imagemin', 'uglify']);
+
+};
